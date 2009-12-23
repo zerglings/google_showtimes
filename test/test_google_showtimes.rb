162bcdaee899a1d278b63827ce2cda971a713b71
Victor Costan <costan@gmail.com>
2009-12-23 04:27:18 -0500

    Initial commit to google_showtimes.

---
+++ test/test_google_showtimes.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestGoogleShowtimes < Test::Unit::TestCase
+  def test_something_for_real
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
